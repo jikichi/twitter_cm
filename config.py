ab51acb823d54cfa5d414022f47029e6ebe15655
高江洲順吉 <jikichi@takaesujunkichinoMacBook-Pro-2.local>
2019-06-25 22:08:58 +0900

    new file

--- config.py
+++
@@ -1,7 +0,0 @@
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
-
-search_id = "" #twitterのscrean_name
-tweetID = "" #tweetID 数字
--- open_fav_list.py
+++
@@ -1,7 +0,0 @@
-def open_fav_list():
-    with open('fav_list.txt') as f2:
-        a = f2.read()
-        even_list = a.split(",")
-        even_list = filter(lambda a: a != "", even_list)
-        new_list = [int(float(x)) for x in even_list]
-    return new_list
