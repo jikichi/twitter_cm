32f7e4ebd0a5e293a484a8e74bada04502a21712
高江洲順吉 <jikichi@takaesujunkichinoMacBook-Pro-2.local>
2019-06-25 22:27:41 +0900

    編集

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-CONSUMER_KEY = "MOf2CyM7iS26Cd8MP5puwnx85"
-CONSUMER_SECRET = "vqeU5p28mJF6e5QtXdq6RwsNqFV0BwsmSWGvm6wFpndpu4EetT"
-ACCESS_TOKEN = "1118728920633712640-X5vYTuBewJVidnjXt08tnRQKiyA0i5"
-ACCESS_TOKEN_SECRET = "9V2MpJqWItpd6OUcIHsymE2c5tE44nXzTnca1hrJVE9OX"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
 
-search_id = "test_jikichi" #ツイッターのID
-tweetID = "1121215938362130432" #tweetID
+search_id = "" #ツイッターのID
+tweetID = "" #tweetID
